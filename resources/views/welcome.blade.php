cf58b1daf3931de0d9e0cc1b88dcd612f5396f32
Ivan Boldyrev <iboldurev@gmail.com>
2020-03-18 21:12:26 +0200

    Added tailwind css support && changed welcome page for use tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,63 +10,12 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <body class="bg-white h-screen text-gray-700 font-sans font-thin">
+        <div class="flex items-center justify-center relative h-screen">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="absolute top-0 right-0 links mr-4 mt-6 links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -79,8 +28,8 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
+            <div class="text-center">
+                <div class="text-7xl mb-6">
                     Laravel
                 </div>
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+      extend: {
+          fontFamily: {
+              'sans': ['Nunito', 'sans-serif']
+          },
+          fontSize: {
+              '7xl': '5rem',
+          }
+      },
+  },
+  variants: {},
+  plugins: [],
+}
